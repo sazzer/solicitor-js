2cf1a54b3911e5086d2394857b2cbf0aa4ce07ce
sazzer <graham@grahamcox.co.uk>
2014-12-23 12:57:53 +0000

    More checks, including testing of advanced checks

--- spec/checks.js
+++ spec/checks.js
@@ -2,13 +2,43 @@ var expect = require('chai').expect,
     solicitor = require('..');
 
 describe('The Solicitor Library', function() {
-    describe('When using pre-defined checks', function() {
-        it('"required" on a defined value', function() {
+    describe('When checking a required value', function() {
+        it('on a defined value', function() {
             solicitor.arg(1).is.required;
         });
-        it('"required" on an undefined value', function() {
+        it('on an undefined value', function() {
             expect(function() {
                 solicitor.arg(undefined).is.required;
+            }).throws();
+        });
+    });
+    describe('When checking for a string', function() {
+        it('on a string', function() {
+            solicitor.arg('Hello').is.a.string;
+        })
+        it('on a number', function() {
+            expect(function() {
+                solicitor.arg(1).is.a.string;
+            }).throws();
+        })
+        it('on undefined', function() {
+            expect(function() {
+                solicitor.arg(undefined).is.a.string;
+            }).throws();
+        })
+    });
+    describe('When checking for a number in a range', function() {
+        it('on a valid number', function() {
+            solicitor.arg(5).is.inRange(1, 10);
+        });
+        it('on a number thats too small', function() {
+            expect(function() {
+                solicitor.arg(0).is.inRange(1, 10);
+            }).throws();
+        });
+        it('on a number thats too big', function() {
+            expect(function() {
+                solicitor.arg(20).is.inRange(1, 10);
             }).throws();
         });
     });
